01367ffd889aff6b42547d8d71c8a70dd387a9db
Mohammed Bajuaifer <mohamadbajuaifer@gmail.com>
2022-10-08 21:29:01 +0300

    create-drf-app

--- create_drf_app/main.py
+++ create_drf_app/main.py
@@ -1,5 +1,11 @@
-import typer
+import os
+import platform
+from re import L
 
+import typer
+from cookiecutter.main import cookiecutter
+
+OS = platform.system()
 
 app = typer.Typer()
 
@@ -9,9 +15,30 @@ def callback():
     DRF scaffold
     """
 
+def get_template_path(template_name):
+    script_path = os.path.abspath(__file__)
+    module_path = os.path.dirname(script_path)
+    project_path = os.path.dirname(module_path)
+    cookiecutters_path = os.path.join(project_path, "templates")
+    return os.path.join(cookiecutters_path, template_name)
+    
+
+def activate_venv():
+
+    parent = os.getcwd()
+    os.system("python -m venv env")
+
+    env_activate_script = ""
+    if OS == "Windows":
+        env_activate_script = "Scripts"
+
+
+    activate_file = os.path.join(parent, "env", env_activate_script, "activate")
+    os.system(f"{activate_file} && pip install -r requirements.txt")
+
 @app.command()
-def main(name):
-    # clone repo
-    # activate virtaulenv
-    # download requirements
-    ...
+def main(template_name):
+    cookiecutter("https://github.com/MohammedBajuaifer/drf-starter-templates.git")
+
+    activate_venv()
+
