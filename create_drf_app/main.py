2e7bf8c788a98993bb2205c0377368a6bf67cf94
Mohammed Bajuaifer <mohamadbajuaifer@gmail.com>
2022-10-09 23:11:03 +0300

    v0.1.0

--- create_drf_app/main.py
+++ create_drf_app/main.py
@@ -17,28 +17,30 @@ def callback():
 
 def get_template_path(template_name):
     script_path = os.path.abspath(__file__)
-    module_path = os.path.dirname(script_path)
-    project_path = os.path.dirname(module_path)
-    cookiecutters_path = os.path.join(project_path, "templates")
-    return os.path.join(cookiecutters_path, template_name)
+    current_dir_path = os.path.dirname(script_path)
+    templates_path = os.path.join(current_dir_path, "templates")
+    return os.path.join(templates_path, template_name)
     
 
-def activate_venv():
+def activate_venv(path):
 
+    # set current directory
+    os.chdir(path)
+    env = os.system("python -m venv env")
     parent = os.getcwd()
-    os.system("python -m venv env")
+    project_path = path
 
     env_activate_script = ""
     if OS == "Windows":
         env_activate_script = "Scripts"
 
-
-    activate_file = os.path.join(parent, "env", env_activate_script, "activate")
+    activate_file = os.path.join(path, "env", env_activate_script, "activate")
     os.system(f"{activate_file} && pip install -r requirements.txt")
 
-@app.command()
-def main(template_name):
-    cookiecutter("https://github.com/MohammedBajuaifer/drf-starter-templates.git")
 
-    activate_venv()
+@app.command("")
+def main(template_name):
+    template_path = get_template_path(template_name)
+    path = cookiecutter(template_path)
+    activate_venv(path)
 
